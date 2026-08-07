81654ebcdcdbb524bce75e0f3d064f077cfe8044
martijnschlosser <martijnschlosser1@gmail.com>
2026-08-07 11:51:49 +0300

    Configure GA4 measurement ID for torremolinos

--- app/site-config.ts
+++ app/site-config.ts
@@ -73,7 +73,7 @@ export const siteConfig = {
     ],
   },
   analytics: {
-    ga4Id: process.env.NEXT_PUBLIC_GA4_MEASUREMENT_ID ?? "",
+    ga4Id: process.env.NEXT_PUBLIC_GA4_MEASUREMENT_ID ?? "G-VFMM3S2E9K",
     googleAdsId: process.env.NEXT_PUBLIC_GOOGLE_ADS_ID ?? "",
     googleAdsConversionLabel:
       process.env.NEXT_PUBLIC_GOOGLE_ADS_CONVERSION_LABEL ?? "",
